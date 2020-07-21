30f0ae54f1c78e8c69fe8541856d9cff7f0265c0
Maksim Likhachev <wwwqwwwqwwwqwwwq@gmail.com>
2020-07-21 10:37:58 +0300

    Create app, register firebase app, add dependencies

---
+++ app/src/main/java/ru/mvlikhachev/firebasechat/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.mvlikhachev.firebasechat;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
