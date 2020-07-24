e7a6ebc496c4349d9c8e95680001cc371204a6d1
Maksim Likhachev <wwwqwwwqwwwqwwwq@gmail.com>
2020-07-24 10:11:20 +0300

    add dependency firebase-auth

--- app/src/main/java/ru/mvlikhachev/firebasechat/SignInActivity.java
+++ app/src/main/java/ru/mvlikhachev/firebasechat/SignInActivity.java
@@ -4,11 +4,24 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignInActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        auth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
     }
 }
